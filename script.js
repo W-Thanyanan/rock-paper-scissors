76520de3e38898caa53104000e221b857d11f4eb
W-Thanyanan <nobc5020@gmail.com>
2025-07-18 04:31:09 +0700

    feat: Create getComputerChoice function

---
+++ script.js
@@ -0,0 +1,12 @@
+function getComputerChoice() {
+  let randomChoice = Math.floor(Math.random() * 3);
+
+  if (randomChoice === 0) {
+    return "Rock";
+  } else if (randomChoice === 1) {
+    return "Paper";
+  } else {
+    return "Scissors";
+  }
+}
+console.log(getComputerChoice());
